ceb294aa3cebe4588e742bdd2dcff9692f3bb9d6
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-01-15 01:26:13 +0530

    login page building

--- src/bank/mangment/system/login.java
+++ src/bank/mangment/system/login.java
@@ -1,5 +1,10 @@
+package bank.mangment.system;
 public class login {
+    login() {
+
+    }
     public static void main(String[] args) {
+
         System.out.println("this is login pager");
     }
 }
